373d7d5e307ca2751a46ef48d6f86eb050b1df91
farayolaj <farayolajoshua@gmail.com>
2019-07-06 22:08:58 +0100

    Chapter finished

--- src/App.js
+++ src/App.js
@@ -24,28 +24,28 @@ function App() {
 	var helloworld = "Welcome to the Road to Learn React";
 	var user = {
 		name: "Joshua",
-		password: "super"
+		password: "sniper"
 	}
   return (
     <div className="App">
       <h1>{helloworld}</h1>
-      <p>You are {user.name} with password...</p>
+      <p>You are {user.name} with password "{user.password}"</p>
       <div>
-        {list.map(function(item) {
-            return (
-                <div key={item.objectID}>
-                    <span>
-                        <a href={item.url}>{item.title}</a>
-                    </span>
-                    <span>{item.author}</span>
-                    <span>{item.num_comments}</span>
-                    <span>{item.points}</span>
-                </div>
-            );
-        })}
+        {list.map(item =>
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>)
+	}
       </div>
     </div>
   );
 }
 
+function Table() {
+}
 export default App;
